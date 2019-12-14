bb3429666186942873c15c1f340837c2dca8433f
Ruud (Rudy) van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2019-12-14 14:26:26 +0100

    Update example.m

--- example.m
+++ example.m
@@ -8,22 +8,21 @@ homedir = mfilename('fullpath'); %folder where this function is stored plus its 
 homedir = homedir(1:end-7); %just the folder where this function is stored 
 addpath(genpath(homedir))
 
-
 %% project the left hemisphere of a volumetric file on an inflated surface, and plot it vertex-wise
 
 fname = [homedir 'volumetric\CHRNB3.nii']; %define file name
 surface_project(fname,'L',1,1,'inflated');
 
-%% project a volumetric file on a very inflated surface, and plot it parcel-wise
+%% project a volumetric file on a very inflated surface, and plot it parcel-wise, and get the data into working memory
 
 [dat, ldat, rdat] = glasserize_nifti(fname,1,'very_inflated'); %surface project and plot
 
 %% plot the myelin map on a sphere parcel-wise
 
 fname = [homedir 'support_files\S1200.MyelinMap.dscalar.nii'];
-[dat, ldat, rdat] = glasserize_cifti(fname,1,'sphere');
+glasserize_cifti(fname,1,'sphere');
 
 %% plot the myelin map on an inflated surface parcel-wise
 
-[dat, ldat, rdat] = glasserize_cifti(fname,1,'inflated');
+glasserize_cifti(fname,1,'inflated');
 
